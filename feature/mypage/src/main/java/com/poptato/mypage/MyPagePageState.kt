238136f1a8f81f112efe32e2c7d7460f35cf087d
dogmania <hj296914@gmail.com>
2025-03-02 22:20:04 +0900

    :sparkles: Feat: 마감기한 날짜 모드 상태 변수 추가

--- feature/mypage/src/main/java/com/poptato/mypage/MyPagePageState.kt
+++ feature/mypage/src/main/java/com/poptato/mypage/MyPagePageState.kt
@@ -7,5 +7,6 @@ data class MyPagePageState(
     val userDataModel: UserDataModel = UserDataModel(),
     val noticeWebViewState: Boolean = false,
     val faqWebViewState: Boolean = false,
-    val policyViewState: Boolean = false
+    val policyViewState: Boolean = false,
+    val isDeadlineDateMode: Boolean = false
 ) : PageState

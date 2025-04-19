eabdfdbc4bdc23fd25859182edd579199afb8808
dogmania <hj296914@gmail.com>
2025-04-19 22:53:03 +0900

    :fire: Remove: taskInput 상태 변수 제거

--- feature/backlog/src/main/java/com/poptato/backlog/BacklogPageState.kt
+++ feature/backlog/src/main/java/com/poptato/backlog/BacklogPageState.kt
@@ -6,7 +6,6 @@ import com.poptato.ui.base.PageState
 
 data class BacklogPageState(
     val backlogList: List<TodoItemModel> = emptyList(),
-    val taskInput: String = "",
     val showTodoBottomSheet: Boolean = false,
     val selectedItem: TodoItemModel = TodoItemModel(),
     val totalItemCount: Int = -1,

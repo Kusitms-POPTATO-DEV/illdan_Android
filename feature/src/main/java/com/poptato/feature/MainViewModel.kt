b53881a7271bba59ee4d225262a6fba295f293af
dogmania <hj296914@gmail.com>
2025-06-08 18:07:59 +0900

    :recycle: Refactor: 바텀시트 비즈니스 로직 뷰모델로 분리

--- feature/src/main/java/com/poptato/feature/MainViewModel.kt
+++ feature/src/main/java/com/poptato/feature/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.poptato.feature
 
 import androidx.lifecycle.viewModelScope
 import com.poptato.core.enums.BottomNavType
+import com.poptato.core.util.DateTimeFormatter
 import com.poptato.domain.model.enums.BottomSheetType
 import com.poptato.domain.model.request.ListRequestModel
 import com.poptato.domain.model.request.todo.RoutineRequestModel
@@ -26,7 +27,7 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val getYesterdayListUseCase: GetYesterdayListUseCase
 ) : BaseViewModel<MainPageState>(MainPageState()) {
-    val todoEventFlow = MutableSharedFlow<TodoExternalEvent>()
+    val todoEventFlow = MutableSharedFlow<TodoExternalEvent>(replay = 1)
     val selectedIconInBottomSheet = MutableSharedFlow<CategoryIconItemModel>()
     val categoryScreenContentFromBacklog = MutableSharedFlow<CategoryScreenContentModel>(replay = 1)
     val userDeleteName = MutableSharedFlow<String>(replay = 1)
@@ -64,95 +65,18 @@ class MainViewModel @Inject constructor(
         val isAllOrStar: Boolean = item.categoryId.toInt() == -1 || item.categoryId.toInt() == 0
         val categoryItemModel = if (isAllOrStar) null else category.firstOrNull { it.categoryId == item.categoryId }
 
-        updateState(
+        updateStateSync(
             uiState.value.copy(
                 selectedTodoItem = item,
                 categoryList = category,
                 selectedTodoCategoryItem = categoryItemModel
             )
         )
-        emitEventFlow(MainEvent.ShowTodoBottomSheet)
-    }
 
-    fun onUpdatedCategory(selectedId: Long?) {
-        val selectedCategory = uiState.value.categoryList.firstOrNull { it.categoryId == selectedId }
-        val updatedTodo = uiState.value.selectedTodoItem.copy(
-            categoryName = selectedCategory?.categoryName ?: "",
-            imageUrl = selectedCategory?.categoryImgUrl ?: ""
-        )
-
-        updateState(
-            uiState.value.copy(
-                selectedTodoCategoryItem = selectedCategory,
-                selectedTodoItem = updatedTodo
-            )
-        )
-    }
-
-    fun onUpdatedDeadline(date: String?) {
-        val updatedItem = uiState.value.selectedTodoItem.copy(deadline = date ?: "")
-
-        updateState(
-            uiState.value.copy(
-                selectedTodoItem = updatedItem
-            )
-        )
-    }
-
-    fun onUpdatedBookmark(value: Boolean) {
-        val updatedItem = uiState.value.selectedTodoItem.copy(isBookmark = value)
-
-        updateState(
-            uiState.value.copy(
-                selectedTodoItem = updatedItem
-            )
-        )
-    }
-
-    fun onUpdatedTodoRepeat(value: Boolean) {
-        val updatedItem = uiState.value.selectedTodoItem.copy(
-            isRepeat = value,
-            routineDays = if (value) emptyList() else uiState.value.selectedTodoItem.routineDays
-        )
-
-        updateState(
-            uiState.value.copy(
-                selectedTodoItem = updatedItem
-            )
-        )
-    }
-
-    fun onUpdatedTodoRoutine(days: Set<Int>?) {
-        val request = RoutineRequestModel()
-        request.convertIndexToDays(days?.toList())
-        val updatedItem = uiState.value.selectedTodoItem.copy(
-            routineDays = request.routineDays ?: emptyList(),
-            isRepeat = if (request.routineDays?.isNotEmpty() == true) false else uiState.value.selectedTodoItem.isRepeat
-        )
-
-        updateState(
-            uiState.value.copy(
-                selectedTodoItem = updatedItem
-            )
-        )
-    }
-
-    fun onUpdatedTodoTime(value: Triple<String, Int, Int>?) {
-        val time = uiState.value.selectedTodoItem.formatTime(value)
-        val updatedItem = uiState.value.selectedTodoItem.copy(time = time)
-
-        updateState(
-            uiState.value.copy(
-                selectedTodoItem = updatedItem
-            )
-        )
-
-        AnalyticsManager.logEvent(eventName = "set_time")
+        updateBottomSheetType(BottomSheetType.Main)
     }
 
     fun toggleBackPressed(value: Boolean) { updateState(uiState.value.copy(backPressedOnce = value)) }
-
-    fun updateBottomSheetType(type: BottomSheetType) { updateState(uiState.value.copy(bottomSheetType = type)) }
 
     fun onSelectedCategoryIcon(categoryList: CategoryIconTotalListModel) {
         updateState(
@@ -161,6 +85,8 @@ class MainViewModel @Inject constructor(
                 categoryIconList = categoryList
             )
         )
+
+        updateBottomSheetType(BottomSheetType.CategoryIcon)
     }
 
     fun onSetDialogContent(dialogContent: DialogContentModel) {
@@ -188,4 +114,143 @@ class MainViewModel @Inject constructor(
         if (route == NavRoutes.TodayScreen.route) { AnalyticsManager.logEvent(eventName = "get_today") }
         else if (route == NavRoutes.HistoryScreen.route) { AnalyticsManager.logEvent(eventName = "get_calendar") }
     }
+
+    // BottomSheetHandler
+    fun updateBottomSheetType(type: BottomSheetType) { updateState(uiState.value.copy(bottomSheetType = type)) }
+
+    fun onDeleteTodo(todoId: Long) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.DeleteTodo(todoId)) }
+        updateBottomSheetType(BottomSheetType.None)
+    }
+
+    fun onModifyTodo(todoId: Long) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.ActiveItem(todoId)) }
+        updateBottomSheetType(BottomSheetType.None)
+    }
+
+    fun onUpdateTodoBookmark(todoId: Long, value: Boolean) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.UpdateBookmark(todoId)) }
+        val updatedItem = uiState.value.selectedTodoItem.copy(isBookmark = value)
+
+        updateState(uiState.value.copy(selectedTodoItem = updatedItem))
+
+        AnalyticsManager.logEvent(
+            eventName = "set_important",
+            params = mapOf("task_ID" to "${uiState.value.selectedTodoItem.todoId}")
+        )
+    }
+
+    fun onUpdatedTodoDeadline(date: String?) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.UpdateDeadline(date)) }
+        val updatedItem = uiState.value.selectedTodoItem.copy(deadline = date ?: "")
+
+        updateState(
+            uiState.value.copy(
+                selectedTodoItem = updatedItem
+            )
+        )
+
+        AnalyticsManager.logEvent(
+            eventName = "set_dday",
+            params = mapOf("set_date" to DateTimeFormatter.getTodayFullDate(), "dday" to "$date", "task_ID" to "${uiState.value.selectedTodoItem.todoId}")
+        )
+    }
+
+    fun onCategoryIconSelected(selectedIcon: CategoryIconItemModel) {
+        viewModelScope.launch { selectedIconInBottomSheet.emit(selectedIcon) }
+        updateBottomSheetType(BottomSheetType.None)
+    }
+
+    fun onCategorySelected(id: Long?) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.UpdateCategory(id)) }
+
+        val selectedCategory = uiState.value.categoryList.firstOrNull { it.categoryId == id }
+        val updatedTodo = uiState.value.selectedTodoItem.copy(
+            categoryName = selectedCategory?.categoryName ?: "",
+            imageUrl = selectedCategory?.categoryImgUrl ?: ""
+        )
+
+        updateState(
+            uiState.value.copy(
+                selectedTodoCategoryItem = selectedCategory,
+                selectedTodoItem = updatedTodo
+            )
+        )
+
+        AnalyticsManager.logEvent(
+            eventName = "set_category"
+        )
+    }
+
+    fun onUpdateTodoTime(todoId: Long, value: Triple<String, Int, Int>?) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.UpdateTime(Pair(todoId, uiState.value.selectedTodoItem.formatTime(value)))) }
+
+        val time = uiState.value.selectedTodoItem.formatTime(value)
+        val updatedItem = uiState.value.selectedTodoItem.copy(time = time)
+
+        updateState(
+            uiState.value.copy(
+                selectedTodoItem = updatedItem
+            )
+        )
+
+        AnalyticsManager.logEvent(eventName = "set_time")
+    }
+
+    fun onUpdateTodoRepeat(todoId: Long, value: Boolean) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.UpdateRepeat(todoId, value)) }
+
+        val updatedItem = uiState.value.selectedTodoItem.copy(
+            isRepeat = value,
+            routineDays = if (value) emptyList() else uiState.value.selectedTodoItem.routineDays
+        )
+
+        updateState(
+            uiState.value.copy(
+                selectedTodoItem = updatedItem
+            )
+        )
+    }
+
+    fun onUpdateTodoRoutine(todoId: Long, days: Set<Int>?) {
+        viewModelScope.launch { todoEventFlow.emit(TodoExternalEvent.UpdateRoutine(todoId, days)) }
+
+        val request = RoutineRequestModel()
+        request.convertIndexToDays(days?.toList())
+        val updatedItem = uiState.value.selectedTodoItem.copy(
+            routineDays = request.routineDays ?: emptyList(),
+            isRepeat = if (request.routineDays?.isNotEmpty() == true) false else uiState.value.selectedTodoItem.isRepeat
+        )
+
+        updateState(
+            uiState.value.copy(
+                selectedTodoItem = updatedItem
+            )
+        )
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

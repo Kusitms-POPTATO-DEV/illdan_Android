1b1c11e815dfd89c8b74accbd59ddef134a934f3
dogmania <hj296914@gmail.com>
2025-06-08 18:05:47 +0900

    :sparkles: Feat: None Type 추가

--- domain/src/main/java/com/poptato/domain/model/enums/BottomSheetType.kt
+++ domain/src/main/java/com/poptato/domain/model/enums/BottomSheetType.kt
@@ -1,5 +1,5 @@
 package com.poptato.domain.model.enums
 
 enum class BottomSheetType{
-    Main, FullDate, CategoryIcon, CategoryList, TimePicker, Routine
+    Main, FullDate, CategoryIcon, CategoryList, TimePicker, Routine, None
 }
